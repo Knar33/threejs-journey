487bbf44c104dd9adfbe27304c6ccc499426a8a2
Knar <knarxr@protonmail.com>
2023-02-02 17:52:56 -0600

    Finished lesson 8

--- 08 Fullscreen and resizing/src/script.js
+++ 08 Fullscreen and resizing/src/script.js
@@ -22,9 +22,40 @@ scene.add(mesh)
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+
+window.addEventListener("resize", () => {
+    //update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    //update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+    
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+window.addEventListener("dblclick", () => {
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement;
+
+    if (!fullscreenElement) {
+        if (canvas.requestFullscreen) {
+            canvas.requestFullscreen();
+        } else {
+            canvas.webkitRequestFullscreen();
+        }
+    } else {
+        if (canvas.requestFullscreen) {
+            document.exitFullscreen();
+        } else {
+            document.webkitExitFullscreen();
+        }
+    };
+})
 
 /**
  * Camera
@@ -45,6 +76,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
