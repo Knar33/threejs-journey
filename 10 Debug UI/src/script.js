4017596d6cd3e58a3affeb331289c1118782075a
Knar <knarxr@protonmail.com>
2023-02-02 20:06:46 -0600

    Lesson 10

--- 10 Debug UI/src/script.js
+++ 10 Debug UI/src/script.js
@@ -1,6 +1,18 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import * as lil from "lil-gui";
+
+const gui = new lil.GUI();
+
+const parameters = {
+    color: 0xff000000,
+    spin: () => {
+        gsap.to(mesh.rotation, {y: mesh.rotation.y + Math.PI * 2, duration: 1})
+    }
+}
+
+gui.add(parameters, 'spin');
 
 /**
  * Base
@@ -18,6 +30,16 @@ const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+gui.add(mesh.position, "y")
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name("elevation");
+
+gui.add(mesh, "visible");
+
+gui.add(material, "wireframe");
 
 /**
  * Sizes
